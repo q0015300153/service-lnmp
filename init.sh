9f8f27b14015ff41b1c80ae0fb627cee91fa1ce1
TB <q0015300153@gmail.com>
2019-08-22 03:43:56 -0400

    Modify the alias folder path

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@
 
 # tw: 建立資料夾
 # en: Create folder
-.env -f ./.env parse WEB_PATH WEB_DATA
+.env -f ./.env parse WEB_PATH WEB_DATA NGINX_ALIAS
 for path in "${REPLY[@]}"; do
 	IFS='=' read -ra param <<< "$path"
 	if ! [[ -n "${param[1]}" ]]; then
@@ -15,6 +15,9 @@ for path in "${REPLY[@]}"; do
 
 	if ! [ -d "${param[1]}" ]; then
 		mkdir -p "$(dirname "$0")/${param[1]}"
+
+		# tw: 建立 index.php
+		# en: Create index.php
 		if [ ${param[0]} = "WEB_PATH" ]; then
 			echo "<!DOCTYPE html>"                  > $(dirname "$0")/${param[1]}/index.php
 			echo "<html>"                          >> $(dirname "$0")/${param[1]}/index.php
@@ -25,6 +28,12 @@ for path in "${REPLY[@]}"; do
 			echo "    <?php phpinfo(); ?>"         >> $(dirname "$0")/${param[1]}/index.php
 			echo "</body>"                         >> $(dirname "$0")/${param[1]}/index.php
 			echo "</html>"                         >> $(dirname "$0")/${param[1]}/index.php
+		fi
+
+		# tw: 複製 nginx/alias/default 設定檔
+		# en: Copy nginx/alias/default setting file
+		if [ ${param[0]} = "NGINX_ALIAS" ]; then
+			cp ./nginx/alias/default $(dirname "$0")/${param[1]}/default
 		fi
 	fi
 done
