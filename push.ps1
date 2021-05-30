0c055456fa602c7b2b205611727b849ab53792bd
TB <q0015300153@gmail.com>
2021-05-30 17:00:33 +0800

    Remove trash

--- push.ps1
+++
@@ -1,4 +0,0 @@
-$path = $(pwd)
-docker run -ti --rm --volume $path/:/git alpine/git add .
-docker run -ti --rm --volume $path/:/git alpine/git commit -m "Add windows support"
-docker run -ti --rm --volume $path/:/git alpine/git push https://github.com/q0015300153/service-lnmp.git master:master
