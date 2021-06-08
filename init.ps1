029e34e3dde8a36e97a31c5e03cb1463987540ca
TB <q0015300153@gmail.com>
2021-06-08 15:15:10 +0800

    add nginx logs folder

--- init.ps1
+++ init.ps1
@@ -8,7 +8,8 @@ Set-PsEnv
 $parse = @(
     @("WEB_PATH",    $Env:WEB_PATH),
     @("WEB_DATA",    $Env:WEB_DATA),
-    @("NGINX_ALIAS", $Env:NGINX_ALIAS)
+    @("NGINX_ALIAS", $Env:NGINX_ALIAS),
+    @("NGINX_LOG",   $Env:NGINX_LOG)
 )
 
 $path = $(pwd)
